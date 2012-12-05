de251b83f9d38e273a62b1daf6662ca680dc7920
SHINJI <hagiwara.shinji@baristride.co.jp>
2012-12-05 21:26:09 +0900

    no message

--- workschedule/header.php
+++ workschedule/header.php
@@ -2,12 +2,16 @@
 <link href="../css/wscss/header.css" rel="stylesheet" type="text/css" />
 <html>
 <input id="menu" type="text"  readonly="readonly" value="MENU" />
-<input id="name" type="text"  readonly="true" value="GUSTO調布上石原店">
-<input id="userid" type="text"  readonly="true" value="GT296928025">
-<input id="username" type="text"  readonly="true" value="萩原　慎司">
-<a id="atag" href="personal.php">PERSONAL SCHEDULE</a>
-<a href="work.php">WORK SCHEDULE</a>
-<a href="member.php">MEMBER</a>
-<a href="scheduling.php">SCHEDULING</a>
-<a href="master.php">MASTER</a>
+<input id="name" type="text"  readonly="readonly" value="GUSTO調布上石原店">
+<input id="userid" type="text"  readonly="readonly" value="GT296928025">
+<input id="username" type="text"  readonly="readonly" value="萩原　慎司">
+<div id="headermenu">
+	<ul>
+		<li><a href="personal.php">PERSONAL<br/>SCHEDULE</a></li>
+		<li><a href="work.php">WORK<br/>SCHEDULE</a></li>
+		<li><a href="member.php">MEMBER</a></li>
+		<li><a href="scheduling.php">SCHEDULING</a></li>
+		<li><a href="master.php">MASTER</a></li>
+	</ul>
+</div>
 </html>
