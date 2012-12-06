2d454705ccddfd8d785f77f70f224510d2fd294c
SHINJI <hagiwara.shinji@baristride.co.jp>
2012-12-07 08:41:11 +0900

    no message

--- workschedule/menu.php
+++ workschedule/menu.php
@@ -1,7 +1,55 @@
 <html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+ charset=utf-8" />
+<title>MENU</title>
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<script type="text/javascript" src="../workschedule/js/headr.js"></script>
+<link href="../workschedule/css/style.css" rel="stylesheet" type="text/css" />
+</head>
 <body>
-<iframe src="header.php" name="header" width="600" height="40" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+<div id="wrapper">
+	<div id="header">
+		<div id="headerinfo">
+		<table id="info" align="center" border="0" cellspacing="0" cellpadding="0">>
+			<tr>
+				<td align="left"　colspan="3"><input id="menu" type="text"  readonly="readonly" value="MENU" /></td>
+			</tr>
+			<tr>
+				<td align="center" colspan="3"><input id="com-bra" type="text"  readonly="readonly" value="GUSTO調布上石原店" /></td>
+			</tr>
+			<tr>
+				<td align="right" colspan="3">
+					<table id="userinfo">
+						<tr><td><input id="userid" type="text"  readonly="readonly" value="GT296928025" /></td></tr>
+						<tr><td><input id="username" type="text"  readonly="readonly" value="萩原　慎司" /></td></tr>
+					</table>
+				</td>
+			</tr>
+		</div>
+			<tr>
+				<div id="headermenu">
+					<table id="atag" border="0" cellspacing="0" cellpadding="0">
+						<tr>
+							<td><a href="personal.php">PERSONAL<br/>SCHEDULE</a></td>
+							<td><a href="work.php">WORK<br/>SCHEDULE</a></td>
+							<td><a href="member.php">MEMBER</a></td>
+							<td><a href="scheduling.php">SCHEDULING</a></td>
+							<td><a href="master.php">MASTER</a></td>
+						</tr>
+					</table>
+				</div>
+			</tr>
+		</table>
+	</div>
 
+	<div id="contents">
+
+	</div>
+
+	<div id="footer">
+	</div>
+</div>
 </body>
 
 </html>
